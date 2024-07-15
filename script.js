df779834af1578963e6a5b2e509da1f189524ff2
jotaviodev <otaviomatosxd3@gmail.com>
2024-07-15 14:53:32 -0300

    New Update: Adding a 'user not found' feature, and adding a new style for the engine

--- script.js
+++ script.js
@@ -9,20 +9,24 @@ async function getData(userName){
     try{
         var request = await fetch(`https://api.github.com/users/${userName}`);
         var data = await request.json()
-        console.log(data)
-    }
-    catch(e){
-        alert("Usuário não encontrado")
-    }
-    box.innerHTML=`
-    <div class="image center">
+        if(data.message=="Not Found"){
+            throw new Error("Usuário não encontrado")
+        }
+        box.innerHTML=`
+        <div class="image center">
         <img src="${data.avatar_url}" alt="GitHubImageProfile">
         </div>
         <div class="infos">
             <h1>${data.name}</h1>
             <h3>@${data.login}</h3>
-
-            <h4>${data.bio}</h4>
+            <h4>Bio: ${data.bio}</h4>
+            <a class="githubLink" href="https://github.com/${data.login}">
+            <i id="githubIcon" class="fa-brands fa-github fa-2xl"></i>
+            </a>
         </div>
     `
+    }
+    catch(e){
+        alert(e)
+    }
 }
